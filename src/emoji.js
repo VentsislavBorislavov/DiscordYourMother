7ecb404954d7461b19f7b8443bb1f233d80d3d60
Vencislav Borislavov <64546764+VentsislavBorislavov@users.noreply.github.com>
2021-12-01 15:45:10 +0200

    emojis

--- src/emoji.js
+++ src/emoji.js
@@ -16,18 +16,8 @@ export const emojis = [
   "🙉",
   "🤢",
   "🥺👉👈",
-  ":WokePepe:",
   ":flushed:",
-  generateGuildEmoji("suure"),
-  generateGuildEmoji("poggers"),
-  generateGuildEmoji("ConceitedReaction"),
-  generateGuildEmoji("realshit"),
-  generateGuildEmoji("iamok"),
-  generateGuildEmoji("PepeYikes"),
-  generateGuildEmoji("huh"),
-  generateGuildEmoji("smileW"),
-  generateGuildEmoji("Cursed"),
-  generateGuildEmoji("quickcheck"),
+  "<:huh:761639990408380417>","<:suure:827170870551511091>>","<:Pog:761637108104036374>>","<:SANDOW:797226660931174450>>","<:booba2:812040156571500565>>","<:MonkaS:761637199795978262>>","💦>","<:poggers:761637071537700885>>","<:WokePepe:761639370670997504>>","<:kappa:788673119333646346>>","<:imOkayChamp:761637401805455401>>","<:iamok:799304088235016252>>",
 ];
 
 // :suure: :poggers: :ConceitedReaction::realshit::iamok::PepeYikes::huh::smileW::Cursed::quickcheck:
