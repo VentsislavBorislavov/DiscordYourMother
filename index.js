395760475917fa33e584f30aaf2160a138ca1c3c
VentsislavBorislavov <vkosevb@gmail.com>
2021-12-01 15:22:13 +0200

    removed useless stuff

--- index.js
+++ index.js
@@ -26,9 +26,6 @@ client.on("ready", () => {
 client.on("messageCreate", (message) => {
   if (message.author.bot) return;
 
-  if(message.content === 'id'){
-    console.log(process.env.GUILD_ID);
-  }
 
   if (shouldReply(message.content, sPhrase)) {
     message.reply(`С майка ти ${randomChoice(emojis)}`);
